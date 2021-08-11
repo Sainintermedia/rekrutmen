098884944fe7ff68c81fd2817c2edb375e5c4ab8
Nfz123 <akholidin0@gmai.com>
2021-08-12 00:22:16 +0700

    AHMAD

--- resources/views/frontend/about.blade.php
+++ resources/views/frontend/about.blade.php
@@ -4,7 +4,8 @@
         <div class="col-md-6">
             <div><h2>Our Team</h2>
             <p>Kerja sama tim atau team building adalah proses dan strategi yang dibangun untuk mewujudkan visi misi TEAM REKRUTMENT. Prinsipnya, kerja sama tim merupakan cara bekerja kreatif berbekalkan komunikasi yang baik dan kemampuan untuk memecahkan masalah yang dihadapi secara bersama-sama.</p>
-            </div>
+            <P>Menjadi bagian dari sebuah tim hebat di tempat kerja tentu menjadi idaman setiap orang, termasuk Anda. Bayangkan jika Anda menjadi bagian dari tim ini, Anda akan bangun pagi dengan penuh semangat dan bekerja dengan penuh energi setiap hari. </P>
+        </div>
             <br/>
         </div>
     </div> 
