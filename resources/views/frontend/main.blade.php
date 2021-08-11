3a920fc94e2cd5b338fcdc2dc63a0aca010c8d34
haris0799 <harispurwana07@gmail.com>
2021-08-11 23:34:33 +0700

    haris

--- resources/views/frontend/main.blade.php
+++ resources/views/frontend/main.blade.php
@@ -7,12 +7,12 @@
 <meta name="description" content="" />
 <meta name="author" content="http://webthemez.com" />
 <!-- css -->
-<link href="css/bootstrap.min.css" rel="stylesheet" />
-<link rel="stylesheet" href="simple-line-icons/css/simple-line-icons.css">
-<link href="css/fancybox/jquery.fancybox.css" rel="stylesheet"> 
-<link href="css/flexslider.css" rel="stylesheet" />
-<link href="js/owl-carousel/owl.carousel.css" rel="stylesheet"> 
-<link href="css/style.css" rel="stylesheet" /> 
+<link href="assets/frontend/css/bootstrap.min.css" rel="stylesheet" />
+<link rel="assets/frontend/stylesheet" href="simple-line-icons/css/simple-line-icons.css">
+<link href="assets/frontend/css/fancybox/jquery.fancybox.css" rel="stylesheet"> 
+<link href="assets/frontend/css/flexslider.css" rel="stylesheet" />
+<link href="assets/frontend/js/owl-carousel/owl.carousel.css" rel="stylesheet"> 
+<link href="assets/frontend/css/style.css" rel="stylesheet" /> 
  
 <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
 <!--[if lt IE 9]>
@@ -51,23 +51,23 @@
     @include('frontend.client')
 	 </section>
 	<footer>
-	@include('frontend.foorter')
+	@include('frontend.footer')
 	</footer>
 </div>
 <a href="#" class="scrollup"><i class="fa fa-angle-up active"></i></a>
 <!-- javascript
     ================================================== -->
 <!-- Placed at the end of the document so the pages load faster -->
-<script src="js/jquery.js"></script>
-<script src="js/jquery.easing.1.3.js"></script>
-<script src="js/bootstrap.min.js"></script>
-<script src="js/jquery.fancybox.pack.js"></script>
-<script src="js/jquery.fancybox-media.js"></script> 
-<script src="js/portfolio/jquery.quicksand.js"></script>
-<script src="js/portfolio/setting.js"></script>
-<script src="js/jquery.flexslider.js"></script>
-<script src="js/animate.js"></script>
-<script src="js/custom.js"></script>
-<script src="js/owl-carousel/owl.carousel.js"></script>
+<script src="assets/frontend/js/jquery.js"></script>
+<script src="assets/frontend/js/jquery.easing.1.3.js"></script>
+<script src="assets/frontend/js/bootstrap.min.js"></script>
+<script src="assets/frontend/js/jquery.fancybox.pack.js"></script>
+<script src="assets/frontend/js/jquery.fancybox-media.js"></script> 
+<script src="assets/frontend/js/portfolio/jquery.quicksand.js"></script>
+<script src="assets/frontend/js/portfolio/setting.js"></script>
+<script src="assets/frontend/js/jquery.flexslider.js"></script>
+<script src="assets/frontend/js/animate.js"></script>
+<script src="assets/frontend/js/custom.js"></script>
+<script src="assets/frontend/js/owl-carousel/owl.carousel.js"></script>
 </body>
 </html>
--- resources/views/frontend/slider.blade.php
+++ resources/views/frontend/slider.blade.php
@@ -1,14 +1,14 @@
 <div id="main-slider" class="flexslider">
     <ul class="slides">
       <li>
-        <img src="img/slides/1.jpg" alt="flexslider" /> 
+        <img src="assets/frontend/img/slides/1.jpg" alt="flexslider" /> 
         <div class="flex-caption">
             <h3>CONSULTING</h3> 
             <p>Doloribus omnis minus temporibus perfer</p>  
         </div> 
       </li>
       <li>
-        <img src="img/slides/2.jpg" alt="flexslider" /> 
+        <img src="assets/frontend/img/slides/2.jpg" alt="flexslider" /> 
         <div class="flex-caption">
             <h3>FINANCIAL</h3> 
             <p>Lorem ipsum dolor sit elitincidunt</p>  
