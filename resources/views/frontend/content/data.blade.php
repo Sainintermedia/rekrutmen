64548b2074b7ce484ed64cf50507cb85121cb714
Nfz123 <akholidin0@gmai.com>
2021-08-13 16:09:54 +0700

    ahmad

--- resources/views/frontend/content/data.blade.php
+++ resources/views/frontend/content/data.blade.php
@@ -5,159 +5,46 @@
     </center>
     <br/>
     <br/>
-            <div class="container">
+    
                 <table class="table table-striped table-bordered data">
                     <thead>
-                        <tr>			
+                        <tr>	
+                            <th>NO</th>		
                             <th>NIK</th>
                             <th>Nama</th>
-                            <th>Pekerjaan</th>
-                            <th>Usia</th>
-                            <th>Status</th>
+                            <th>Tempat Lahir</th>
+                            <th>Tgl Lahir</th>
+                            <th>Jenis Kelamin</th>
+                            <th>Kampung</th>
+                            <th>RT</th>
+                            <th>RW</th>
+                            <th>Pelamar</th>
+                            <th>Telepon / WA</th>
+                            <th>Pendidikan</th>
+                            <th>Paklaring</th>
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>				
-                            <td>Andi</td>
-                            <td>Jakarta</td>
-                            <td>Web Designer</td>
-                            <td>21</td>
-                            <td>Aktif</td>
+                         @foreach( $bendung as $bd)
+                        <tr>
+                        
+                            <th scope="row">{{ $loop->iteration }}</th>
+                            <td>{{$bd->nik}}</td>
+                            <td>{{$bd->nama}}</td>
+                            <td>{{$bd->tempat_lahir}}</td>
+                            <td>{{$bd->tgl_lahir}}</td>
+                            <td>{{$bd->jeniskelamin}}</td>
+                            <td>{{$bd->kampung}}</td>
+                            <td>{{$bd->rt}}</td>
+                            <td>{{$bd->rw}}</td>
+                            <td>{{$bd->pelamar}}</td>
+                            <td>{{$bd->telepon}}</td>
+                            <td>{{$bd->pendidikan}}</td>
+                            <td>{{$bd->paklaring}}</td>
                         </tr>
-                        <tr>				
-                            <td>Malas Ngoding</td>
-                            <td>Bandung</td>
-                            <td>Web Developer</td>
-                            <td>26</td>
-                            <td>Aktif</td>
-                        </tr>
-                        <tr>				
-                            <td>Malas Ngoding</td>
-                            <td>Bandung</td>
-                            <td>Web Developer</td>
-                            <td>26</td>
-                            <td>Aktif</td>
-                        </tr>
-                        <tr>				
-                            <td>Andi</td>
-                            <td>Jakarta</td>
-                            <td>Web Designer</td>
-                            <td>21</td>
-                            <td>Aktif</td>
-                        </tr>
-                        <tr>				
-                            <td>aman</td>
-                            <td>Jakarta</td>
-                            <td>Web Designer</td>
-                            <td>21</td>
-                            <td>Aktif</td>
-                        </tr>
-                        <tr>				
-                            <td>Andi</td>
-                            <td>Jakarta</td>
-                            <td>Web Designer</td>
-                            <td>21</td>
-                            <td>Aktif</td>
-                        </tr>
-                        <tr>				
-                            <td>Andi</td>
-                            <td>Jakarta</td>
-                            <td>Web Designer</td>
-                            <td>21</td>
-                            <td>Aktif</td>
-                        </tr>
-                        <tr>				
-                            <td>Andi</td>
-                            <td>Jakarta</td>
-                            <td>Web Designer</td>
-                            <td>21</td>
-                            <td>Aktif</td>
-                        </tr>
-                        <tr>				
-                            <td>Andi</td>
-                            <td>Jakarta</td>
-                            <td>Web Designer</td>
-                            <td>21</td>
-                            <td>Aktif</td>
-                        </tr>
-                        <tr>				
-                            <td>Andi</td>
-                            <td>Jakarta</td>
-                            <td>Web Designer</td>
-                            <td>21</td>
-                            <td>Aktif</td>
-                        </tr>
-                        <tr>				
-                            <td>Andi</td>
-                            <td>Jakarta</td>
-                            <td>Web Designer</td>
-                            <td>21</td>
-                            <td>Aktif</td>
-                        </tr>
-                        <tr>				
-                            <td>Andi</td>
-                            <td>Jakarta</td>
-                            <td>Web Designer</td>
-                            <td>21</td>
-                            <td>Aktif</td>
-                        </tr>
-                        <tr>				
-                            <td>Andi</td>
-                            <td>Jakarta</td>
-                            <td>Web Designer</td>
-                            <td>21</td>
-                            <td>Aktif</td>
-                        </tr>
-                        <tr>				
-                            <td>Andi</td>
-                            <td>Jakarta</td>
-                            <td>Web Designer</td>
-                            <td>21</td>
-                            <td>Aktif</td>
-                        </tr>
-                        <tr>				
-                            <td>Andi</td>
-                            <td>Jakarta</td>
-                            <td>Web Designer</td>
-                            <td>21</td>
-                            <td>Aktif</td>
-                        </tr>
-                        <tr>				
-                            <td>Andi</td>
-                            <td>Jakarta</td>
-                            <td>Web Designer</td>
-                            <td>21</td>
-                            <td>Aktif</td>
-                        </tr>
-                        <tr>				
-                            <td>Andi</td>
-                            <td>Jakarta</td>
-                            <td>Web Designer</td>
-                            <td>21</td>
-                            <td>Aktif</td>
-                        </tr>
-                        <tr>				
-                            <td>Andi</td>
-                            <td>Jakarta</td>
-                            <td>Web Designer</td>
-                            <td>21</td>
-                            <td>Aktif</td>
-                        </tr>
-                        <tr>				
-                            <td>Andi</td>
-                            <td>Jakarta</td>
-                            <td>Web Designer</td>
-                            <td>21</td>
-                            <td>Aktif</td>
-                        </tr>
-                        <tr>				
-                            <td>Andi</td>
-                            <td>Jakarta</td>
-                            <td>Web Designer</td>
-                            <td>21</td>
-                            <td>Aktif</td>
-                        </tr>
+                        @endforeach
                     </tbody>
+                 
                 </table>
-            </div>
+          
 @endsection
