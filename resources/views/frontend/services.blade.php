7f49ea213a5bfe521bb121597873467b6935b3da
Nfz123 <akholidin0@gmai.com>
2021-08-13 09:45:37 +0700

    ahmad

--- resources/views/frontend/services.blade.php
+++ resources/views/frontend/services.blade.php
@@ -5,32 +5,32 @@
 		<div class="col-md-3 text-center">
 		<div class="box"> 
 		<span class="icons c1"><i class="icon-settings icons"></i></span> <div class="box-area">
-		<h3>Sumurbandung</h3> <p>Lorem ipsum dolor sit amet,</p> <p><a href="#">Learn More</a></p></div>
+		<h3>Sumurbandung</h3> <p>Lorem ipsum dolor sit amet,</p> <p><a href="#">Read More</a></p></div>
 		</div></div>
 		<div class="col-md-3 text-center"> 
 		<div class="box"> 
 		<span class="icons c2"><i class="icon-login icons"></i></span> <div class="box-area">
-		<h3>Kramat</h3> <p>Lorem ipsum dolor sit amet,</p> <p><a href="#">Learn More</a></p></div>
+		<h3>Kramat</h3> <p>Lorem ipsum dolor sit amet,</p> <p><a href="#">Read More</a></p></div>
 		</div></div>
 		<div class="col-md-3 text-center"> 
 		<div class="box"> 
 		<span class="icons c3"><i class="icon-user icons"></i></span> <div class="box-area">
-		<h3>Kunir</h3> <p>Lorem ipsum dolor sit amet,</p> <p><a href="#">Learn More</a></p></div>
+		<h3>Kunir</h3> <p>Lorem ipsum dolor sit amet,</p> <p><a href="#">Read More</a></p></div>
 		</div></div>
 		<div class="col-md-3 text-center"> 
 		<div class="box">
 		<span class="icons c4"><i class="icon-home icons"></i></span> <div class="box-area">
-		<h3>Bendung</h3> <p>Lorem ipsum dolor sit amet,</p> <p><a href="#">Learn More</a></p></div>
+		<h3>Bendung</h3> <p>Lorem ipsum dolor sit amet,</p> <p><a href="#">Read More</a></p></div>
 		</div></div>
 		<div class="col-md-3 text-center"> 
 		<div class="box">
 		<span class="icons c4"><i class="icon-home icons"></i></span> <div class="box-area">
-		<h3>Sempur</h3> <p>Lorem ipsum dolor sit amet,</p> <p><a href="#">Learn More</a></p></div>
+		<h3>Sempur</h3> <p>Lorem ipsum dolor sit amet,</p> <p><a href="#">Read More</a></p></div>
 		</div></div>
 		<div class="col-md-3 text-center"> 
 		<div class="box">
 		<span class="icons c4"><i class="icon-home icons"></i></span> <div class="box-area">
-		<h3>Saradan</h3> <p>Lorem ipsum dolor sit amet,</p> <p><a href="#">Learn More</a></p></div>
+		<h3>Saradan</h3> <p>Lorem ipsum dolor sit amet,</p> <p><a href="#">Read More</a></p></div>
 		</div></div>
 		</div></div></div>
 		</div> 
